12d70d06e739cf060c7cd69017c1eb7f4452bef7
Franco Speziali <franco.speziali@digitalcareerinstitute.org>
2021-12-08 16:39:38 +0100

    rewrote jwt helpers for auth

--- authentication/helpers/jwt.js
+++ authentication/helpers/jwt.js
@@ -1,16 +1,37 @@
 const jsonwebtoken = require("jsonwebtoken");
+const { Promise } = require("mongoose");
 
 // must pass in whole user document
-exports.issueJwt = async (user) => {
+exports.issueJwt = (user) => {
   const payload = {
     id: user._id,
   };
 
-  return await jsonwebtoken.sign(payload, process.env["SECRET"], {
-    expiresIn: "1h",
+  return new Promise((resolve, reject) => {
+    jsonwebtoken.sign(
+      payload,
+      process.env["SECRET"],
+      {
+        expiresIn: "1h",
+      },
+      (error, token) => {
+        if (error) {
+          reject();
+        }
+        resolve(token);
+      }
+    );
   });
 };
 
 exports.verifyJwt = async (token) => {
-  return jsonwebtoken.verify(token, process.env["SECRET"]);
+  return new Promise((resolve, reject) => {
+    jsonwebtoken.verify(token, process.env["SECRET"], (err, decoded) => {
+      if (err) {
+        reject();
+      }
+
+      resolve(decoded);
+    });
+  });
 };
